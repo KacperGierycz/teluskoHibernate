ab6c52cbbe7d358ac1bfdcaf21e45e6d80dd584b
Kacper <kacpergierycz@gmail.com>
2021-05-14 07:22:32 +0200

    Maven quick start initialize and alien created

---
+++ DemoHib/src/main/java/telusko/DemoHib/App.java
@@ -0,0 +1,20 @@
+package telusko.DemoHib;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	
+    	Alien kacper = new Alien();
+    	kacper.setid(37);
+    	kacper.setName("kacper");
+    	kacper.setColor("green");
+    	
+    	
+    	
+    }
+}
