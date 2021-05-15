68904cffd0701b748e78b6ae994a35c37bc40afc
Kacper <kacpergierycz@gmail.com>
2021-05-15 09:18:19 +0200

    Hibernate Relations Student and laptop

--- DemoHib/src/main/java/telusko/DemoHib/App.java
+++ DemoHib/src/main/java/telusko/DemoHib/App.java
@@ -1,7 +1,5 @@
 package telusko.DemoHib;
 
-
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -9,14 +7,42 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ){
+    	//AlienDataBase();    	
+    	StudentLaptop();
+    }    
+
+    
+    public static void StudentLaptop() {
+    	
+    	Laptop laptop= new Laptop();
+    	laptop.setLid(101);
+    	laptop.setLname("Dell");
+    	
+    	Student s = new Student();
+    	s.setName("Kacper");
+    	s.setSid(1);
+    	s.setMarks(50);
+    	
+    	
+    	Configuration con = new Configuration().configure().addAnnotatedClass(Student.class).addAnnotatedClass(Laptop.class);    	
+    	ServiceRegistry reg= new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();   	   	
+    	SessionFactory sf=con.buildSessionFactory(reg); 	
+    	Session session=sf.openSession();
+    	
+    	Transaction tx=session.beginTransaction();
+    	
+    	session.save(laptop);
+    	session.save(s);
+    	
+    	session.getTransaction().commit();
+    }
+    
+    
+    public static void AlienDataBase() {
+    	
     	AlienName an=new AlienName();
     	an.setFname("Tytus");
     	an.setLname("Kacper");
@@ -29,14 +55,9 @@ public class App
     	kacper.setColor("red");
     	
     	
-    	Configuration con = new Configuration().configure().addAnnotatedClass(Alien.class);
-    	
-    	
-    	ServiceRegistry reg= new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();
-    	
-    	
-    	SessionFactory sf=con.buildSessionFactory(reg);
-    	
+    	Configuration con = new Configuration().configure().addAnnotatedClass(Alien.class);    	
+    	ServiceRegistry reg= new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();   	   	
+    	SessionFactory sf=con.buildSessionFactory(reg); 	
     	Session session=sf.openSession();
     	
     	Transaction tx=session.beginTransaction();
@@ -50,6 +71,7 @@ public class App
     	tx.commit();
     	
     	System.out.println(kacper);
-    
+    	
     }
+    
 }
